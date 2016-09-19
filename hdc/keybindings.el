cc3c051d20592ffd86fa99274c91c3a10cdaf8db
huangdecai <249967141@qq.com>
2016-09-19 17:31:08 +0800

    news

--- hdc/keybindings.el
+++ hdc/keybindings.el
@@ -22,4 +22,11 @@
 
 (global-set-key (kbd "s-/") 'hippie-expand)
 
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-n") #'company-select-next)
+  (define-key company-active-map (kbd "C-p") #'company-select-previous))
+  
+
 (global-set-key (kbd "<f2>") 'open-init-file)
